9ccab9c99ac644294c75bb586868f6ea59b05647
zer0 <osom8979@gmail.com>
2021-07-19 20:26:12 +0900

    Remove unused js files.

--- fe/src/settings/theme/theme_default.js
+++
@@ -1,9 +0,0 @@
-export default {
-    primary: '#3f51b5',
-    secondary: '#b0bec5',
-    accent: '#8c9eff',
-    error: '#b71c1c',
-    warning: "#ffeb3b",
-    info: "#ff96f3",
-    success: "#ffaf50"
-}
--- fe/src/settings/theme/theme_test.js
+++
@@ -1,9 +0,0 @@
-export default {
-    primary: "#004336",
-    secondary: "#007373",
-    accent: "#0027b0",
-    error: "#004336",
-    warning: "#00eb3b",
-    info: "#0096f3",
-    success: "#00af50"
-}
