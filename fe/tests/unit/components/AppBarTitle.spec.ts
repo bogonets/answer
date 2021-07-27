4186e6a915ae7d1c0df52426bcb9319b8763180a
zer0 <osom8979@gmail.com>
2021-07-27 20:26:39 +0900

    Test component: AppBarTitle

---
+++ fe/tests/unit/components/AppBarTitle.spec.ts
@@ -0,0 +1,34 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import AppBarTitle from '@/components/AppBarTitle.vue';
+
+
+describe('AppBarTitle.vue', () => {
+
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('Default rendering', () => {
+
+    const testTitle = 'aaa';
+    const testSubtitle = 'bbb';
+
+    const wrapper = mount(AppBarTitle, {
+      localVue,
+      vuetify,
+      propsData: {
+        title: testTitle,
+        subtitle: testSubtitle,
+      },
+    });
+
+    expect(wrapper.props('title')).toMatch(testTitle);
+    expect(wrapper.props('subtitle')).toMatch(testSubtitle);
+    expect(wrapper.props('flat')).toBeFalsy();
+    console.log(wrapper.html());
+  })
+})
