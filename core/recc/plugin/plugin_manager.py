9dc805ff4989628c5d3354510484b9d34e7b33d9
zer0 <osom8979@gmail.com>
2021-09-13 14:07:14 +0900

    Create class: PluginManager

---
+++ core/recc/plugin/plugin_manager.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from typing import Dict, Any
+from recc.plugin.plugin import Plugin
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+
+
+class PluginManager(Dict[str, Plugin]):
+    def exists_setup(self, name: str) -> bool:
+        return self.__getitem__(name).exists_setup()
+
+    def exists_teardown(self, name: str) -> bool:
+        return self.__getitem__(name).exists_teardown()
+
+    def exists_request(self, name: str) -> bool:
+        return self.__getitem__(name).exists_request()
+
+    async def call_setup(self, name: str, context: Any) -> None:
+        await self.__getitem__(name).call_setup(context)
+
+    async def call_teardown(self, name: str) -> None:
+        await self.__getitem__(name).call_teardown()
+
+    async def call_request(self, name: str, request: Request) -> Response:
+        return await self.__getitem__(name).call_request(request)
