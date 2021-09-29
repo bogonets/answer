1f67a4ea25c2ce8c7ec1019345bb2ae10e92f95f
zer0 <osom8979@gmail.com>
2021-09-29 13:54:49 +0900

    Remove @vue/composition-api, vue-chart-3

--- fe/src/plugins/chart.js
+++ fe/src/plugins/chart.js
@@ -1,8 +1,3 @@
-import Vue from 'vue';
-
-import VueCompositionAPI from '@vue/composition-api';
-Vue.use(VueCompositionAPI);
-
 import {Chart, registerables} from 'chart.js';
 import 'chartjs-adapter-moment';
 import ChartStreaming from 'chartjs-plugin-streaming';
