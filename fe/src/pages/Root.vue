b404128e54d8a01fc0dacc485264d2cf581eecea
zer0 <osom8979@gmail.com>
2021-09-29 18:58:32 +0900

    Update page: Root

--- fe/src/pages/Root.vue
+++ fe/src/pages/Root.vue
@@ -1,14 +1,46 @@
+<i18n lang="yaml">
+en:
+  groups: "Groups"
+  projects: "Projects"
+
+ko:
+  groups: "Groups"
+  projects: "Projects"
+</i18n>
+
 <template>
   <v-container>
+
+    <span class="ml-2 text--secondary text-subtitle-2">
+      {{ $t('groups') }}
+    </span>
+    <v-divider></v-divider>
     <table-groups
+        hide-filter-input
+        hide-new-item-button
         hide-action-edit
         hide-action-move
         clickable-row
         :loading="loading"
-        :items="items"
-        @click:new="onClickNew"
-        @click:row="onClickRow"
+        :items="groups"
+        @click:row="onClickGroupRow"
     ></table-groups>
+
+    <span class="ml-2 text--secondary text-subtitle-2">
+      {{ $t('projects') }}
+    </span>
+    <v-divider></v-divider>
+    <table-projects
+        hide-filter-input
+        hide-new-item-button
+        hide-action-edit
+        hide-action-move
+        clickable-row
+        :loading="loading"
+        :items="projects"
+        @click:row="onClickProjectRow"
+    ></table-projects>
+
   </v-container>
 </template>
 
@@ -16,16 +48,20 @@
 import {Component} from 'vue-property-decorator';
 import VueBase from '@/base/VueBase';
 import TableGroups from '@/components/TableGroups.vue';
+import TableProjects from '@/components/TableProjects.vue';
 import {GroupA} from "@/packet/group";
+import {ProjectA} from "@/packet/project";
 
 @Component({
   components: {
     TableGroups,
+    TableProjects,
   }
 })
 export default class Root extends VueBase {
   loading = false;
-  items = [] as Array<GroupA>;
+  groups = [] as Array<GroupA>;
+  projects = [] as Array<ProjectA>;
 
   created() {
     if (!this.$localStore.alreadySession) {
@@ -48,7 +84,8 @@ export default class Root extends VueBase {
       const user = await this.$api2.getSelf();
       this.$sessionStore.permissionAdmin = user.is_admin || false;
 
-      this.items = await this.$api2.getMainGroups();
+      this.groups = await this.$api2.getMainGroups();
+      this.projects = await this.$api2.getMainProjects();
     } catch (error) {
       this.toastRequestFailure(error);
     } finally {
@@ -56,12 +93,12 @@ export default class Root extends VueBase {
     }
   }
 
-  onClickNew() {
-    this.moveToRootGroupsNew();
+  onClickGroupRow(item: GroupA) {
+    this.moveToGroupProjects(item.slug);
   }
 
-  onClickRow(item: GroupA) {
-    this.moveToGroupProjects(item.slug);
+  onClickProjectRow(item: ProjectA) {
+    this.moveToMain(item.group_slug, item.project_slug);
   }
 }
 </script>
