5dd1cd2c99f2199be7b34df9ab54d504dd24bba6
zer0 <osom8979@gmail.com>
2021-10-12 19:55:50 +0900

    Fix ContextMenu popup error

--- fe/src/services/visualgraph/graph/ContextMenu.js
+++ fe/src/services/visualgraph/graph/ContextMenu.js
@@ -34,7 +34,10 @@
                 }
             }
 
-            if (options.event && options.event.constructor !== MouseEvent && options.event.constructor !== CustomEvent) {
+            if (options.event &&
+                options.event.constructor !== MouseEvent &&
+                options.event.constructor !== CustomEvent &&
+                options.event.constructor !== PointerEvent) {
                 console.error("Event passed to ContextMenu is not of type MouseEvent or CustomEvent. Ignoring it.");
                 options.event = null;
             }
