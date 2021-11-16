a8c69f60a39e4ff28025b04d43469c01e9aaab25
zer0 <osom8979@gmail.com>
2021-11-16 16:29:43 +0900

    Update pytest configs

--- core/pytest.sh
+++ core/pytest.sh
@@ -3,10 +3,21 @@
 CORE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
 RECC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.."; pwd)
 
-"$RECC_DIR/python" -m pytest \
-    -v \
-    --cov \
-    --cov-report=term-missing \
-    --cov-report=html \
-    --cov-config="${CORE_DIR}/pytest.ini" \
-    "$@"
+STORAGE_DIR=$CORE_DIR/storage
+DAEMON_DIR=$STORAGE_DIR/daemon
+PLUGIN_DIR=$STORAGE_DIR/plugin
+
+for i in $(find "$DAEMON_DIR" -mindepth 1 -maxdepth 1 -type d); do
+    PYTHONPATH="$i:$PYTHONPATH"
+done
+for i in $(find "$PLUGIN_DIR" -mindepth 1 -maxdepth 1 -type d); do
+    PYTHONPATH="$i:$PYTHONPATH"
+done
+
+PYTHONPATH="$PYTHONPATH" "$RECC_DIR/python" -m pytest \
+     -v \
+     --cov \
+     --cov-report=term-missing \
+     --cov-report=html \
+     --cov-config="${CORE_DIR}/pytest.ini" \
+     "$@"
