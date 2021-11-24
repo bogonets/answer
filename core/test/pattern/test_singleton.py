188b29b50b03d6ab3113cdf2f1463d6fc81c8f31
zer0 <osom8979@gmail.com>
2021-11-24 17:01:23 +0900

    Create module: recc.pattern.singleton

---
+++ core/test/pattern/test_singleton.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from unittest import main, TestCase
+from recc.pattern.singleton import singleton
+
+
+@singleton
+class _TestSingleton:
+    def __init__(self):
+        self.value = 0
+
+
+class SingletonTestCase(TestCase):
+    def test_default(self):
+        a = _TestSingleton()
+        b = _TestSingleton()
+        self.assertEqual(id(a), id(b))
+        self.assertEqual(0, a.value)
+        self.assertEqual(0, b.value)
+
+        a.value += 1
+        self.assertEqual(1, a.value)
+        self.assertEqual(1, b.value)
+
+        b.value += 1
+        self.assertEqual(2, a.value)
+        self.assertEqual(2, b.value)
+
+
+if __name__ == "__main__":
+    main()
