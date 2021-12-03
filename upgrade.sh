0377ef1de48ffc319d6fde3fb949b118e5ce0e79
zer0 <osom8979@gmail.com>
2021-12-03 09:45:31 +0900

    Move dir: core/package -> packaging

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-RECC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
+RECC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
 VERSION=$(cat "$RECC_DIR/VERSION")
 
-DEBIAN_VERSION_PATH="$RECC_DIR/core/package/debian/DEBIAN/control"
+DEBIAN_VERSION_PATH="$RECC_DIR/packaging/debian/DEBIAN/control"
 CORE_VERSION_PATH="$RECC_DIR/core/recc/util/version.py"
 FE_VERSION_PATH="$RECC_DIR/fe/package.json"
 
@@ -11,7 +11,6 @@ sed -i.tmp -e "s/Version: .*/Version: $VERSION/" "$DEBIAN_VERSION_PATH"
 sed -i.tmp -e "s/version_text = \".*\"/version_text = \"$VERSION\"/" "$CORE_VERSION_PATH"
 sed -i.tmp -e "s/  \"version\": \".*\",/  \"version\": \"$VERSION\",/" "$FE_VERSION_PATH"
 
-rm $DEBIAN_VERSION_PATH.tmp
-rm $CORE_VERSION_PATH.tmp
-rm $FE_VERSION_PATH.tmp
-
+rm "$DEBIAN_VERSION_PATH.tmp"
+rm "$CORE_VERSION_PATH.tmp"
+rm "$FE_VERSION_PATH.tmp"
