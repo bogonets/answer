a31404d6d760acca20ec6f57fa586f2a1b593d57
zer0 <osom8979@gmail.com>
2021-12-03 15:59:44 +0900

    Fix script warnings in requirements.sh

--- core/requirements.sh
+++ core/requirements.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-CORE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
-RECC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.."; pwd)
+CORE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
+RECC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit; pwd)
 
 "$RECC_DIR/python" -m pip install --upgrade pip
 
-for f in $CORE_DIR/requirements.*.txt; do
+for f in "$CORE_DIR"/requirements.*.txt; do
     "$RECC_DIR/python" -m pip install -r "$f"
 done
-
