31fcacc4fb6d469cfbb20c4db6674814301dd31b
zer0 <osom8979@gmail.com>
2022-02-16 18:00:47 +0900

    Update version: 2.0.0-dev9

--- core/recc/util/version.py
+++ core/recc/util/version.py
@@ -10,7 +10,7 @@ def normalize_version(ver: str) -> str:
     return ver.replace("-", ".")
 
 
-version_text = "2.0.0-dev8"
+version_text = "2.0.0-dev9"
 version_info = parse_semantic_version(version_text)
 
 database_version = version_text.split("-")[0]
