c45f526f983a6011e1e79e3cb0e2b7098fe62794
zer0 <osom8979@gmail.com>
2022-03-03 12:59:43 +0900

    Create method: parse_rpc_address

--- core/recc/uri/rpc_uri.py
+++ core/recc/uri/rpc_uri.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from typing import NamedTuple, Optional, Tuple
+from typing import NamedTuple, Optional, Tuple, List, Union
+from socket import AF_INET, AF_INET6
 from enum import Enum
-from recc.uri.host_port import parse_host_port
+from recc.uri.host_port import (
+    parse_ipv4_or_domain_port,
+    parse_ipv6_port,
+    parse_host_port,
+    OPENING_IPV6,
+    CLOSING_IPV6,
+)
+from recc.network.address_family import get_ip_address_family
 
 
 class RpcSchemeType(Enum):
@@ -17,12 +25,14 @@ NOT_FOUND_INDEX = -1
 
 SCHEME_DNS = "dns:"
 SCHEME_UNIX = "unix:"
+SCHEME_UNIX2 = SCHEME_UNIX + "//"
 SCHEME_UNIX_ABSTRACT = "unix-abstract:"
 SCHEME_IPV4 = "ipv4:"
 SCHEME_IPV6 = "ipv6:"
 
 SCHEME_DNS_LEN = len(SCHEME_DNS)
 SCHEME_UNIX_LEN = len(SCHEME_UNIX)
+SCHEME_UNIX2_LEN = len(SCHEME_UNIX2)
 SCHEME_UNIX_ABSTRACT_LEN = len(SCHEME_UNIX_ABSTRACT)
 SCHEME_IPV4_LEN = len(SCHEME_IPV4)
 SCHEME_IPV6_LEN = len(SCHEME_IPV6)
@@ -81,9 +91,8 @@ class RpcDnsAddress(NamedTuple):
         return host_port[1] if host_port is not None else None
 
 
-def parse_rpc_dns_address(address: str) -> RpcDnsAddress:
-    if get_rpc_scheme_type(address) != RpcSchemeType.Dns:
-        raise ValueError(f"Invalid DNS scheme: {address}")
+def _parse_rpc_dns_address(address: str) -> RpcDnsAddress:
+    assert get_rpc_scheme_type(address) == RpcSchemeType.Dns
 
     authority: Optional[str]
 
@@ -110,3 +119,163 @@ def parse_rpc_dns_address(address: str) -> RpcDnsAddress:
 
     host_port = parse_host_port(host_port_body)
     return RpcDnsAddress(authority, host_port.host, host_port.port)
+
+
+def parse_rpc_dns_address(address: str) -> RpcDnsAddress:
+    if get_rpc_scheme_type(address) != RpcSchemeType.Dns:
+        raise ValueError(f"Invalid DNS scheme: {address}")
+    return _parse_rpc_dns_address(address)
+
+
+class RpcUnixAddress(NamedTuple):
+    """
+    Syntax: ``unix:path`` or ``unix://absolute_path``
+    """
+
+    path: str
+
+
+def _parse_rpc_unix_address(address: str) -> RpcUnixAddress:
+    assert get_rpc_scheme_type(address) == RpcSchemeType.Unix
+
+    # [IMPORTANT]
+    # Do not change the order of if-else statements.
+    if address.startswith(SCHEME_UNIX2):
+        absolute_path = address[SCHEME_UNIX2_LEN:]
+        if absolute_path[0] != "/":
+            raise ValueError(
+                f"If you use the `{SCHEME_UNIX2}` prefix,"
+                f"the path must be absolute: {address}"
+            )
+        return RpcUnixAddress(absolute_path)
+    elif address.startswith(SCHEME_UNIX):
+        return RpcUnixAddress(address[SCHEME_UNIX_LEN:])
+    else:
+        assert False, "Inaccessible section"
+
+
+def parse_rpc_unix_address(address: str) -> RpcUnixAddress:
+    if get_rpc_scheme_type(address) != RpcSchemeType.Unix:
+        raise ValueError(f"Invalid Unix scheme: {address}")
+    return _parse_rpc_unix_address(address)
+
+
+class RpcUnixAbstractAddress(NamedTuple):
+    """
+    Syntax: ``unix-abstract:abstract_path``
+    """
+
+    abstract_path: str
+
+
+def _parse_rpc_unix_abstract_address(address: str) -> RpcUnixAbstractAddress:
+    assert get_rpc_scheme_type(address) == RpcSchemeType.UnixAbstract
+    return RpcUnixAbstractAddress(address[SCHEME_UNIX_ABSTRACT_LEN:])
+
+
+def parse_rpc_unix_abstract_address(address: str) -> RpcUnixAbstractAddress:
+    if get_rpc_scheme_type(address) != RpcSchemeType.UnixAbstract:
+        raise ValueError(f"Invalid Unix Abstract scheme: {address}")
+    return _parse_rpc_unix_abstract_address(address)
+
+
+class Address(NamedTuple):
+    address: str
+    port: Optional[int]
+
+
+class RpcIpv4Addresses(NamedTuple):
+    """
+    Syntax: ``ipv4:address[:port][,address[:port],...]``
+    """
+
+    addresses: List[Address]
+
+
+class RpcIpv6Addresses(NamedTuple):
+    """
+    Syntax: ``ipv6:address[:port][,address[:port],...]``
+    """
+
+    addresses: List[Address]
+
+
+assert RpcIpv4Addresses != RpcIpv6Addresses, "They must be of different types"
+ADDRESS_SEPARATOR = ","
+
+
+def _parse_rpc_ipv4_addresses(address: str) -> RpcIpv4Addresses:
+    assert get_rpc_scheme_type(address) == RpcSchemeType.Ipv4
+
+    addresses = list()
+    body = address[SCHEME_IPV4_LEN:]
+    for ipv4_address in body.split(ADDRESS_SEPARATOR):
+        host_port = parse_ipv4_or_domain_port(ipv4_address)
+        host = host_port.host
+        port = host_port.port
+
+        if get_ip_address_family(host) != AF_INET:
+            raise ValueError(f"Invalid IPv4 address included: {address}")
+
+        addresses.append(Address(host, port))
+
+    return RpcIpv4Addresses(addresses)
+
+
+def parse_rpc_ipv4_addresses(address: str) -> RpcIpv4Addresses:
+    if get_rpc_scheme_type(address) != RpcSchemeType.Ipv4:
+        raise ValueError(f"Invalid IPv4 scheme: {address}")
+    return _parse_rpc_ipv4_addresses(address)
+
+
+def _parse_rpc_ipv6_addresses(address: str) -> RpcIpv6Addresses:
+    assert get_rpc_scheme_type(address) == RpcSchemeType.Ipv6
+
+    addresses = list()
+    body = address[SCHEME_IPV6_LEN:]
+    for ipv6_address in body.split(ADDRESS_SEPARATOR):
+        host_port = parse_ipv6_port(ipv6_address)
+        host = host_port.host
+        port = host_port.port
+
+        assert host[0] == OPENING_IPV6
+        assert host[-1] == CLOSING_IPV6
+        remove_square_bracket_host = host[1:-1]
+
+        if get_ip_address_family(remove_square_bracket_host) != AF_INET6:
+            raise ValueError(f"Invalid IPv6 address included: {address}")
+
+        addresses.append(Address(host, port))
+
+    return RpcIpv6Addresses(addresses)
+
+
+def parse_rpc_ipv6_addresses(address: str) -> RpcIpv6Addresses:
+    if get_rpc_scheme_type(address) != RpcSchemeType.Ipv6:
+        raise ValueError(f"Invalid IPv6 scheme: {address}")
+    return _parse_rpc_ipv6_addresses(address)
+
+
+UnionRpcAddress = Union[
+    RpcDnsAddress,
+    RpcUnixAddress,
+    RpcUnixAbstractAddress,
+    RpcIpv4Addresses,
+    RpcIpv6Addresses,
+]
+
+
+def parse_rpc_address(address: str) -> Tuple[RpcSchemeType, UnionRpcAddress]:
+    scheme_type = get_rpc_scheme_type(address)
+    if scheme_type == RpcSchemeType.Dns:
+        return RpcSchemeType.Dns, _parse_rpc_dns_address(address)
+    elif scheme_type == RpcSchemeType.Unix:
+        return RpcSchemeType.Unix, _parse_rpc_unix_address(address)
+    elif scheme_type == RpcSchemeType.UnixAbstract:
+        return RpcSchemeType.UnixAbstract, _parse_rpc_unix_abstract_address(address)
+    elif scheme_type == RpcSchemeType.Ipv4:
+        return RpcSchemeType.Ipv4, _parse_rpc_ipv4_addresses(address)
+    elif scheme_type == RpcSchemeType.Ipv6:
+        return RpcSchemeType.Ipv6, _parse_rpc_ipv6_addresses(address)
+    else:
+        assert False, "Inaccessible section"
--- core/test/uri/test_rpc_uri.py
+++ core/test/uri/test_rpc_uri.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase, main
-from recc.uri.rpc_uri import parse_rpc_dns_address
+from recc.uri.rpc_uri import (
+    RpcSchemeType,
+    RpcDnsAddress,
+    RpcUnixAddress,
+    RpcUnixAbstractAddress,
+    RpcIpv4Addresses,
+    RpcIpv6Addresses,
+    parse_rpc_dns_address,
+    parse_rpc_unix_address,
+    parse_rpc_unix_abstract_address,
+    parse_rpc_ipv4_addresses,
+    parse_rpc_ipv6_addresses,
+    parse_rpc_address,
+)
 
 
 class RpcAddressParseTestCase(TestCase):
@@ -83,6 +96,102 @@ class RpcAddressParseTestCase(TestCase):
         self.assertIsNone(dns7.authority_port)
         self.assertEqual("[::1]", dns7.host)
         self.assertEqual(1234, dns7.port)
+
+    def test_unix(self):
+        unix1 = parse_rpc_unix_address("unix:/var/recc.sock")
+        self.assertEqual("/var/recc.sock", unix1.path)
+
+        unix2 = parse_rpc_unix_address("unix:./recc.sock")
+        self.assertEqual("./recc.sock", unix2.path)
+
+        unix3 = parse_rpc_unix_address("unix:///var/recc.sock")
+        self.assertEqual("/var/recc.sock", unix3.path)
+
+        with self.assertRaises(ValueError):
+            parse_rpc_unix_address("unix://./recc.sock")
+
+    def test_unix_abstract(self):
+        unix_abs = parse_rpc_unix_abstract_address("unix-abstract:unknown.sock")
+        self.assertEqual("unknown.sock", unix_abs.abstract_path)
+
+    def test_ipv4(self):
+        ip1 = parse_rpc_ipv4_addresses("ipv4:192.168.0.1,0.0.0.0:9999")
+        self.assertEqual(2, len(ip1.addresses))
+        self.assertEqual("192.168.0.1", ip1.addresses[0].address)
+        self.assertIsNone(ip1.addresses[0].port)
+        self.assertEqual("0.0.0.0", ip1.addresses[1].address)
+        self.assertEqual(9999, ip1.addresses[1].port)
+
+        ip2 = parse_rpc_ipv4_addresses("ipv4:127.0.0.1:0")
+        self.assertEqual(1, len(ip2.addresses))
+        self.assertEqual("127.0.0.1", ip2.addresses[0].address)
+        self.assertEqual(0, ip2.addresses[0].port)
+
+        with self.assertRaises(ValueError):
+            parse_rpc_ipv4_addresses("ipv4:localhost")
+
+    def test_ipv6(self):
+        ip1 = parse_rpc_ipv6_addresses("ipv6:[2607:f8b0:400e:c00::ef],[::]:1234")
+        self.assertEqual(2, len(ip1.addresses))
+        self.assertEqual("[2607:f8b0:400e:c00::ef]", ip1.addresses[0].address)
+        self.assertIsNone(ip1.addresses[0].port)
+        self.assertEqual("[::]", ip1.addresses[1].address)
+        self.assertEqual(1234, ip1.addresses[1].port)
+
+        ip2 = parse_rpc_ipv6_addresses("ipv6:[::1]")
+        self.assertEqual(1, len(ip2.addresses))
+        self.assertEqual("[::1]", ip2.addresses[0].address)
+        self.assertIsNone(ip2.addresses[0].port)
+
+        with self.assertRaises(ValueError):
+            parse_rpc_ipv6_addresses("ipv6:localhost")
+
+    def test_parse_rpc_address(self):
+        addr1 = parse_rpc_address("dns://dns-server:53/localhost:9999")
+        self.assertEqual(RpcSchemeType.Dns, addr1[0])
+        self.assertIsInstance(addr1[1], RpcDnsAddress)
+        self.assertEqual("dns-server:53", addr1[1].authority)
+        self.assertEqual("dns-server", addr1[1].authority_host)
+        self.assertEqual(53, addr1[1].authority_port)
+        self.assertEqual("localhost", addr1[1].host)
+        self.assertEqual(9999, addr1[1].port)
+
+        addr2 = parse_rpc_address("[::1]:1234")
+        self.assertEqual(RpcSchemeType.Dns, addr2[0])
+        self.assertIsInstance(addr2[1], RpcDnsAddress)
+        self.assertIsNone(addr2[1].authority)
+        self.assertIsNone(addr2[1].authority_host)
+        self.assertIsNone(addr2[1].authority_port)
+        self.assertEqual("[::1]", addr2[1].host)
+        self.assertEqual(1234, addr2[1].port)
+
+        addr3 = parse_rpc_address("unix:/var/recc.sock")
+        self.assertEqual(RpcSchemeType.Unix, addr3[0])
+        self.assertIsInstance(addr3[1], RpcUnixAddress)
+        self.assertEqual("/var/recc.sock", addr3[1].path)
+
+        addr4 = parse_rpc_address("unix-abstract:unknown.sock")
+        self.assertEqual(RpcSchemeType.UnixAbstract, addr4[0])
+        self.assertIsInstance(addr4[1], RpcUnixAbstractAddress)
+        self.assertEqual("unknown.sock", addr4[1].abstract_path)
+
+        addr5 = parse_rpc_address("ipv4:192.168.0.1,0.0.0.0:9999")
+        self.assertEqual(RpcSchemeType.Ipv4, addr5[0])
+        self.assertIsInstance(addr5[1], RpcIpv4Addresses)
+        self.assertEqual(2, len(addr5[1].addresses))
+        self.assertEqual("192.168.0.1", addr5[1].addresses[0].address)
+        self.assertIsNone(addr5[1].addresses[0].port)
+        self.assertEqual("0.0.0.0", addr5[1].addresses[1].address)
+        self.assertEqual(9999, addr5[1].addresses[1].port)
+
+        addr6 = parse_rpc_address("ipv6:[2607:f8b0:400e:c00::ef],[::]:1234")
+        self.assertEqual(RpcSchemeType.Ipv6, addr6[0])
+        self.assertIsInstance(addr6[1], RpcIpv6Addresses)
+        self.assertEqual(2, len(addr6[1].addresses))
+        self.assertEqual("[2607:f8b0:400e:c00::ef]", addr6[1].addresses[0].address)
+        self.assertIsNone(addr6[1].addresses[0].port)
+        self.assertEqual("[::]", addr6[1].addresses[1].address)
+        self.assertEqual(1234, addr6[1].addresses[1].port)
 
 
 if __name__ == "__main__":
