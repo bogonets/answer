58b63bf86d9220a58131ad8534f1c7f5b1747d34
zer0 <osom8979@gmail.com>
2022-04-14 14:40:15 +0900

    Update api version

--- upgrade.sh
+++ upgrade.sh
@@ -5,12 +5,15 @@ VERSION=$(cat "$RECC_DIR/VERSION")
 
 DEBIAN_VERSION_PATH="$RECC_DIR/packaging/debian/DEBIAN/control"
 CORE_VERSION_PATH="$RECC_DIR/core/recc/util/version.py"
+API_VERSION_PATH="$RECC_DIR/api/package.json"
 FE_VERSION_PATH="$RECC_DIR/fe/package.json"
 
 sed -i.tmp -e "s/Version: .*/Version: $VERSION/" "$DEBIAN_VERSION_PATH"
 sed -i.tmp -e "s/version_text = \".*\"/version_text = \"$VERSION\"/" "$CORE_VERSION_PATH"
+sed -i.tmp -e "s/  \"version\": \".*\",/  \"version\": \"$VERSION\",/" "$API_VERSION_PATH"
 sed -i.tmp -e "s/  \"version\": \".*\",/  \"version\": \"$VERSION\",/" "$FE_VERSION_PATH"
 
 rm "$DEBIAN_VERSION_PATH.tmp"
 rm "$CORE_VERSION_PATH.tmp"
+rm "$API_VERSION_PATH.tmp"
 rm "$FE_VERSION_PATH.tmp"
