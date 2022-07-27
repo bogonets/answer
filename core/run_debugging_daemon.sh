c5f911205a01e0a19a14223e9c9f667f80e66593
zer0 <osom8979@gmail.com>
2022-07-27 10:26:39 +0900

    Fix cmd-args in run_debugging_daemon.sh

--- core/run_debugging_daemon.sh
+++ core/run_debugging_daemon.sh
@@ -13,18 +13,7 @@ if [[ -z $DAEMON_PLUGIN_NAME ]]; then
     exit 1
 fi
 
-DAEMON_PLUGIN_DIR="$STORAGE_DIR/daemon/$DAEMON_PLUGIN_NAME"
-DAEMON_PLUGIN_SCRIPT_PATH="$DAEMON_PLUGIN_DIR/$DAEMON_PLUGIN_NAME.py"
-DAEMON_VENV_DIR="$DAEMON_PLUGIN_DIR/.venv"
-DAEMON_SITE_PACKAGES_DIR="$DAEMON_VENV_DIR/lib/python3.8/site-packages"
-
-if [[ ! -f "$DAEMON_PLUGIN_SCRIPT_PATH" ]]; then
-    echo "Not exists plugin file: $DAEMON_PLUGIN_SCRIPT_PATH"
-    exit 1
-fi
-
 "$RECC_DIR/python" "$CORE_DIR/main.py" -vv -d \
     daemon \
     --daemon-address "$DAEMON_ADDRESS" \
-    --daemon-file "$DAEMON_PLUGIN_SCRIPT_PATH" \
-    --daemon-packages-dir "$DAEMON_SITE_PACKAGES_DIR"
+    --daemon-module "$DAEMON_PLUGIN_NAME"
