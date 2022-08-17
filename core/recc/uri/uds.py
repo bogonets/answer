694a1a5443dcb16e2f0b2b0063efff5b1aefd902
zer0 <osom8979@gmail.com>
2022-08-17 12:52:51 +0900

    Trivial commit

--- core/recc/uri/uds.py
+++ core/recc/uri/uds.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-# UDS = Unix Domain Socket
 
 from recc.variables.rpc import UNIX_ABSTRACT_URI_PREFIX, UNIX_URI_PREFIX
 
 
 def is_uds_family(address: str) -> bool:
+    """
+    Make sure it is a Unix Domain Socket (UDS) address.
+    """
     if address.startswith(UNIX_URI_PREFIX):
         return True
     if address.startswith(UNIX_ABSTRACT_URI_PREFIX):
