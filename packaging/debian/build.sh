2ca44f564c5aa2b95dca7a087abf493b25a32d59
zer0 <osom8979@gmail.com>
2022-08-24 16:34:10 +0900

    Remove packaging dir

--- packaging/debian/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
-PACKAGE_NAME=$(grep --color=never 'Package: .*' "$SCRIPT_DIR/DEBIAN/control" | sed 's/Package: //')
-PACKAGE_VERSION=$(grep --color=never 'Version: .*' "$SCRIPT_DIR/DEBIAN/control" | sed 's/Version: //')
-PACKAGE_FILE="${PACKAGE_NAME}_${PACKAGE_VERSION}.deb"
-
-dpkg-deb --build "$SCRIPT_DIR" "$PACKAGE_FILE"
