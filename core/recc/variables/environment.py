1159e383e2a7349e84fdde0dbc8ecdbe8c756c55
zer0 <osom8979@gmail.com>
2022-10-14 10:15:04 +0900

    Remove unused variables

--- core/recc/variables/environment.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-RECC_ENV_PREFIX = "RECC_"
-RECC_ENV_FILE_SUFFIX = "_FILE"
--- core/recc/variables/session.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-SESSION_ACCESS_TOKEN_DURATION = "10 minutes"
-SESSION_REFRESH_TOKEN_DURATION = "1 hour"
