b91efff79a170a0df06259f2c5919d4d5d54f367
zer0 <osom8979@gmail.com>
2022-10-20 10:26:27 +0900

    Create clean script

---
+++ core/clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
+
+rm -vrf \
+    "$ROOT_DIR/.mypy_cache/" \
+    "$ROOT_DIR/.pytest_cache/" \
+    "$ROOT_DIR/build/" \
+    "$ROOT_DIR/dist/" \
+    "$ROOT_DIR/recc.egg-info/"
